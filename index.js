1b0edab8d825f5c1c80ba626e4e586b3306919dc
Pawan Sharma <pawansharma93520@gmail.com>
2024-02-29 22:20:08 +0530

    Event listner fixed but some work pending

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-onload = alert("Hello love birds");
-function afterClick(){
-    let parsentage = Math.floor(Math.random*100);
-    return document.getElementsByClassName(main-heading).innerHTML("your partener gave you" + parsentage+ "% love.");
-   
+function loveCal() {
+  var randomLove = Math.floor(Math.random() * 100);
+  return alert("Your Partner love You " + randomLove + "%");
 }
-document.querySelector('.final-btn').addEventListener('click',afterClick()
-);
+
+document.querySelector("button").addEventListener("click", loveCal);
