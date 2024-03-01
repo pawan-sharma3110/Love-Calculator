e80280356648bec57109c3c81395f63b2356cd8a
Pawan Sharma <pawansharma93520@gmail.com>
2024-03-01 21:37:31 +0530

    outpufixed in h2

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function loveCal() {
   var randomLove = Math.floor(Math.random() * 100);
-  return alert("Your Partner love You " + randomLove + "%");
-}
+  document.querySelector("h2").style.display = "block";
+  document.querySelector("h1").style.display = "none";
+  return (document.querySelector("h2").textContent =
+    "Your Partner love You " + randomLove + "%");
+  
+  }
+
 
 document.querySelector("button").addEventListener("click", loveCal);
